e3a205a4aaa057a14436f2ec5af0707dd2a124af
JM10PRO <108186529+JM10PRO@users.noreply.github.com>
2022-09-23 15:49:43 +0200

    Add files via upload

--- 03-01-OperadoresJS.js
+++ 03-01-OperadoresJS.js
@@ -17,3 +17,27 @@ console.log("Resto de la división (módulo) " + z);
 
 z = a**b;
 console.log("Resultado de operador exponente 3 elevado a 2 = " + z);
+
+//Incremento
+
+// pre-incremento (el operador ++ antes de la variable)
+z = ++a;
+console.log(a);
+console.log(z);
+
+// post-incremento (el operador ++ después de la variable)
+z = b++;
+console.log(b);
+console.log(z);
+
+//Decremento
+
+// predecremento
+z = --a;
+console.log(a);
+console.log(z);
+
+// postdecremento
+z = b--;
+console.log(b);
+console.log(z);
