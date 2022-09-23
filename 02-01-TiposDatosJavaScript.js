9215fac412aa76c2bfd8562b6ae7bc888779ea93
JM10PRO <108186529+JM10PRO@users.noreply.github.com>
2022-09-23 14:00:20 +0200

    Add files via upload

---
+++ 02-01-TiposDatosJavaScript.js
@@ -0,0 +1,82 @@
+/*
+Ejemplo de 
+tipos de datos
+en JavaScript
+
+Las variables son dinámicas, es decir, no hay que especificar el tipo de variable
+*/
+// Tipo de dato string
+var nombre = 'Jose';
+console.log(typeof nombre);
+
+nombre = 10.5;
+console.log(typeof nombre);
+
+// Tipo de dato numérico
+var numero = 1000;
+console.log(numero);
+
+// Tipo de dato object
+var objeto = {
+    nombre: "Jose",
+    apellido: "Gil",
+    tlfn: "554433221"
+};
+console.log(objeto);
+
+// Tipo de dato boolean (true, false)
+var bandera = true;
+console.log(typeof bandera);
+
+// Tipo de dato function
+function miFuncion(){}
+console.log(typeof miFuncion);
+
+// Tipo de dato Symbol
+var simbolo = Symbol("Mi símbolo");
+console.log(simbolo);
+
+// Tipo clase es una function
+class Persona{
+    constructor(nombre, apellido){
+        this.nombre = nombre;
+        this.apellido = apellido;
+    }
+}
+console.log(typeof Persona);
+
+// Tipo undefined
+var x;
+console.log(x);
+
+x = undefined;
+console.log(typeof x);
+
+// null = ausencia de valor  (tipo object)
+var y = null;
+console.log(typeof y);
+
+// array
+var coches = ["BMW","Audi","Volvo"];
+console.log(coches);
+console.log(typeof coches);
+
+var z = '';
+console.log(z);
+console.log(typeof z);
+
+let nombre1 = 'Jose';
+let apellido = 'Gil';
+let nombreCompleto = nombre1 +' '+ apellido;
+console.log(nombreCompleto);
+
+let j = nombre1 + 3 + 24;
+console.log(j);
+
+// prioridad a los paréntesis
+j = nombre1 + (2+7);
+console.log(j);
+
+// se evalúa de izq a derecha
+j = 2 + 7 + nombre1;
+console.log(j);
---
+++ 03-01-OperadoresJS.js
@@ -0,0 +1,19 @@
+let a = 3;
+let b = 2;
+let z = a+b;
+console.log("Resultado de la suma " + z);
+
+z = a-b;
+console.log("Resultado de la resta " + z);
+
+z = a*b;
+console.log("Resultado de la multiplicación " + z);
+
+z = a/b;
+console.log("Resultado de la división " + z);
+
+z = a%b;
+console.log("Resto de la división (módulo) " + z);
+
+z = a**b;
+console.log("Resultado de operador exponente 3 elevado a 2 = " + z);
